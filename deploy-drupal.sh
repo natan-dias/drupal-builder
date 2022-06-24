ae8657cf9718667e0706ea8de8dfa2dd267d3104
natan-dias <natan.dias1@live.com>
2022-06-24 19:39:37 -0300

    add script

--- deploy-drupal.sh
+++ deploy-drupal.sh
@@ -2,3 +2,12 @@
 echo Set the name of the new site:
 read sitename
 echo Your new Drupal site name is $sitename
+echo Starting deploy Drupal instance...
+sleep 3
+echo Deploy Namespace
+if [cat drupal/namespace.yml | sed "s/{{sitename}}/$sitename/g" | kubectl apply -f -]; then
+     echo “Success”
+else
+     echo “Failure, exit status: $?”
+fi
+
