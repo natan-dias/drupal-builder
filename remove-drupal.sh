0f12f64bc57c33de5c5478dc8f45f99f1e61bf02
natan-dias <natan.dias1@live.com>
2022-06-27 20:14:24 -0300

    change script

--- remove-drupal.sh
+++ remove-drupal.sh
@@ -9,7 +9,7 @@ read sitename
 
 testname=$(microk8s.kubectl get namespaces --field-selector metadata.name=$sitename -o=name)
 
-if testname != null ; then
+if [testname == namespace/$sitename] ; then
      echo Your removed Drupal site name is $sitename
 else
      echo Site Not Found! Nothing will be removed, exit status: $?
