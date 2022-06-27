67e0194639ea82b78c32225bfd7ef45affeca2b3
natan-dias <natan.dias1@live.com>
2022-06-27 19:24:59 -0300

    change script with secrets for MySQL

--- deploy-drupal.sh
+++ deploy-drupal.sh
@@ -27,6 +27,14 @@ else
 fi
 sleep 3
 
+echo ==== Deploy MYSQL Instance Secrets ====
+if cat mysql/secret.yml | sed "s/{{sitename}}/$sitename/g" | sed "s/{{password}}/$password/g" |  microk8s.kubectl apply -f -; then
+     echo “Success”
+else
+     echo “Failure, exit status: $?”
+fi
+sleep 3
+
 echo ==== Deploy MYSQL Instance Storage ====
 if cat mysql/storage.yml | sed "s/{{sitename}}/$sitename/g" | microk8s.kubectl apply -f -; then
      echo “Success”
@@ -93,13 +101,13 @@ microk8s.kubectl get pvc -n $sitename -o wide
 echo ===== DATABASE INFORMATION FOR DRUPAL SETUP =====
 DATABASE_NAME=$(microk8s.kubectl get deploy $sitename-mysql -n $sitename -o jsonpath='{.spec.template.spec.containers[*].env[1].value}')
 MYSQL_PASSWORD=$(microk8s.kubectl get secret $sitename-mysql-secret -o jsonpath='{.data.password}' | base64 -d)
-SERVICE_HOST_NAME=$(microk8s.kubectl get svc -n $sitename -o jsonpath='{.items[1].metadata.name}')
+SERVICE_HOST_NAME=$(microk8s.kubectl get svc -n $sitename -o jsonpath='{.items[0].metadata.name}')
 MYSQL_PORT=$(microk8s.kubectl get svc $sitename-mysql-service -n $sitename -o jsonpath='{.spec.ports[*].port}')
 
 #IF Use password with clear text: MYSQL_PASSWORD=$(microk8s.kubectl get deploy $sitename-mysql -n $sitename -o jsonpath='{.spec.template.spec.containers[*].env[0].value}')
 
-echo Database Name is $DATABASE_NAME
-echo Database username is ROOT
-echo Password is $MYSQL_PASSWORD
-echo Host MySQL is $SERVICE_HOST_NAME
-echo MySQL Port is $MYSQL_PORT
+echo Database Name is ==> $DATABASE_NAME
+echo Database username is ==> ROOT
+echo Password is ==> $MYSQL_PASSWORD
+echo Host MySQL is ==> $SERVICE_HOST_NAME
+echo MySQL Port is ==> $MYSQL_PORT
